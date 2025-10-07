753703a27bc4eefceb1072da5f4ee125f0256bc8
Karim-Ayd <Karim.Ayada@student.htw-berlin.de>
2025-10-07 14:21:58 +0200

    1

---
+++ src/main/java/com/example/demo/TaskBuddy2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBuddy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBuddy2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/TaskBuddy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBuddy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
